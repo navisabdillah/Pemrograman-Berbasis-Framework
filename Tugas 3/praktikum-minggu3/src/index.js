ff91c6e7e59ef78ae8fa038efb0d964ea6c3f32e
navisabdillah <mavisabdi13@gmail.com>
2022-03-09 20:47:25 +0700

    StateFullComponent

--- Tugas 3/praktikum-minggu3/src/index.js
+++ Tugas 3/praktikum-minggu3/src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import HelloComponent from './HelloComponent';
+
+
+//ReactDOM.render(
+ // <React.StrictMode>
+    //<App />
+  //</React.StrictMode>,
+  //document.getElementById('root')
+//);
+
+class StateFullComponent extends React.Component{
+  render(){
+    return <p>StateFullComponent</p>
+  }
+}
+
+ReactDOM.render(<StateFullComponent/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
