126b551b1f35b4a69efc08ab3782478637730fa5
navisabdillah <mavisabdi13@gmail.com>
2022-03-23 19:50:51 +0700

    Praktikum 3

--- Tugas 5/minggu5/src/App.js
+++ Tugas 5/minggu5/src/App.js
@@ -6,7 +6,8 @@ import {
   Link,
   Redirect,
   useHistory,
-  useParams
+  useParams,
+  useRouteMatch
 } from "react-router-dom";
 
 // export default function BasicExample() {
@@ -69,42 +70,119 @@ import {
 //     }
 
    // Praktikum 2
-export default function BasicExample() {
+// export default function BasicExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <h2>Accounts</h2>
+//         <ul>
+//           <li>
+//             <Link to="/netflix">Netflix</Link>
+//           </li>
+//           <li>
+//             <Link to="/gmail">Gmail</Link>
+//           </li>
+//           <li>
+//             <Link to="/yahoo">Yahoo</Link>
+//           </li>
+//           <li>
+//             <Link to="/amazon">Amazon</Link>
+//           </li>
+//         </ul>
+
+//         <Switch>
+//           <Route path="/:id" children={<Child />} />
+//           </Switch>
+//         </div>
+//         </Router>
+//   );
+// }
+
+// function Child(){
+//   let {id} = useParams();
+
+//   return(
+//     <div>
+//       <h3>ID : {id}</h3>
+//     </div>
+//   );
+// }
+
+//Prak 3
+export default function NestingExample() {
   return (
     <Router>
       <div>
-        <h2>Accounts</h2>
         <ul>
           <li>
-            <Link to="/netflix">Netflix</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/gmail">Gmail</Link>
+            <Link to="/topics">Topics</Link>
           </li>
-          <li>
-            <Link to="/yahoo">Yahoo</Link>
-          </li>
-          <li>
-            <Link to="/amazon">Amazon</Link>
-          </li>
-        </ul>
+          </ul>
+          <hr />
 
-        <Switch>
-          <Route path="/:id" children={<Child />} />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/topics">
+              <Topics/>
+            </Route>
           </Switch>
-        </div>
-        </Router>
+          </div>
+          </Router>
   );
 }
 
-function Child(){
-  let {id} = useParams();
-
+function Home (){
   return(
     <div>
-      <h3>ID : {id}</h3>
+      <h2>Home</h2>
     </div>
   );
 }
 
+function Topics(){
+//   //'path' untuk membuat jalur <Route> yang terhadap rute induk,
+//   //sedangkan 'url' untuk membuat link
+
+  let {path, url} = useRouteMatch();
+  return(
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={'${url}/Sate, Nasi goreng'}>Kuliner</Link>
+        </li>
+        <li>
+          <Link to={'${url}/Wisata alam, Museum'}>Travelling</Link>
+        </li>
+        <li>
+          <Link to={'${url}/Iblis, JW Marriot'}>Review Hotel</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={'${path}/:topicId'}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic(){
+  let { topicId } = useParams();
+
+  return(
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
   
