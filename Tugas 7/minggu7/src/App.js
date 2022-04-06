a59a292117a6abb2488c02b4a05b27204ab260c5
navisabdillah <mavisabdi13@gmail.com>
2022-04-06 19:39:49 +0700

    Update pada App.js

--- Tugas 7/minggu7/src/App.js
+++ Tugas 7/minggu7/src/App.js
@@ -1,12 +1,14 @@
 import { Component } from "react";
 import './App.css'
 import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
 
 class App extends Component {
   render() {
     return(
       <div className="App">
         <ClickCounter />
+        <HoverCounter/>
       </div>
     )
   }
