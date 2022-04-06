01851acbed11dfd3d20aace56191d7260513e758
navisabdillah <mavisabdi13@gmail.com>
2022-04-06 19:22:55 +0700

    Membuat ClickCounter.js

---
+++ Tugas 7/minggu7/src/components/ClickCounter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ClickCounter extends Component {
+    render() {
+        return  <button>Click X times </button>      
+             
+                  
+    }
+}
+
+export default ClickCounter
