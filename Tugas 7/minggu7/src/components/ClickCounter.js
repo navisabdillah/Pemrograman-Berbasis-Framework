771cd4ee3cd8d3c63b947724959d54085d4a84ea
navisabdillah <mavisabdi13@gmail.com>
2022-04-06 19:30:00 +0700

    Menambahkan fungsi incrementCount

--- Tugas 7/minggu7/src/components/ClickCounter.js
+++ Tugas 7/minggu7/src/components/ClickCounter.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 
 class ClickCounter extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    incrementCount = () => {
+        this.setState(prevState => {
+            return { count: prevState.count + 1}
+        })
+    }
     render() {
-        return  <button>Click X times </button>      
+        const { count } = this.state
+        return  <button onClick={this.incrementCount}>Click {count} times </button>      
              
                   
     }
