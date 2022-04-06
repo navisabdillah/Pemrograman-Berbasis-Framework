677083f8fb46cc347bf49f1e346057ca6558f887
navisabdillah <mavisabdi13@gmail.com>
2022-04-06 20:10:03 +0700

    Update pada ClickCounter

--- Tugas 7/minggu7/src/components/ClickCounter.js
+++ Tugas 7/minggu7/src/components/ClickCounter.js
@@ -2,23 +2,12 @@ import React, { Component } from 'react'
 import UpdateComponent from './withCounter'
 
 class ClickCounter extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            count: 0
-        }
-    }
-
-    incrementCount = () => {
-        this.setState(prevState => {
-            return { count: prevState.count + 1}
-        })
-    }
-    render() {
-        const { count } = this.state
-        return  <button onClick={this.incrementCount}>{this.props.name}Click {count} times </button>      
-             
+    
+    render() {        
+        const { count, incrementCount} = this.props
+        return  <button onClick={incrementCount}>
+         Click {count} times </button>      
+    
                   
     }
 }
