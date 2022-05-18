fae3b67f098ec93c40a9376a227e6f31d1485f4e
navisabdillah <mavisabdi13@gmail.com>
2022-05-18 21:59:11 +0700

    Praktikum

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BlogPost from './container/BlogPost/BlogPost';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const Hello = () =>{
+//   return <p>Hello World</p>
+// }
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(<BlogPost/>, document.getElementById('content'));
+
+// ReactDOM.render(
+//   <p>Hello World</p>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
