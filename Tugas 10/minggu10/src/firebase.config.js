71fdf9a9da108025263ed29c7f93d7353601136c
navisabdillah <mavisabdi13@gmail.com>
2022-05-24 21:15:27 +0700

    config firebase

---
+++ Tugas 10/minggu10/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyA66h7OyZ9I0YYByAviqJnobH0i5a-ic8Y",
+  authDomain: "latihan-4551a.firebaseapp.com",
+  projectId: "latihan-4551a",
+  storageBucket: "latihan-4551a.appspot.com",
+  messagingSenderId: "471238677340",
+  appId: "1:471238677340:web:0ad4c48b47e40acc34e799",
+  measurementId: "G-PT0NCRXK34"
+};
