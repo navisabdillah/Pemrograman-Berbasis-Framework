9d32f12ada1a6888ab387ff22397df57f4305381
navisabdillah <mavisabdi13@gmail.com>
2022-05-24 21:16:14 +0700

    Edit pada App.js

--- Tugas 10/minggu10/src/App.js
+++ Tugas 10/minggu10/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import routes from './routes';
+import Header from './Header';
+import { BrowserRouter } from 'react-router-dom';
+
+export const AuthContext = React.createContext(null)
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+      Is Logged in ? {JSON.stringify(isLoggedIn)}    
+      <div className='App'>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            {
+              routes.map(route => (
+                <Route 
+                  key={route.path}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main}
+                />
+              ))
+            }
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
